845bdb775d73957f035e1aa3ebf13af0e2251595
Julian Rodriguez <92952502+juleanrod@users.noreply.github.com>
2022-12-01 02:48:34 -0800

    Add implementation 2022 / day 1

---
+++ 2022/day1/day1.js
@@ -0,0 +1,43 @@
+// need to grab data from file
+const fs = require('fs').promises;
+
+// part1
+//const maxCaloriesPerElf = async (name) => {
+    //let data = await fs.readFile(name, "utf8");
+    //const caloriesMatrix = data.split('\n\n').map(collection => collection.split('\n'));
+    //let elfWithMostCalories;
+    //let maxCalories = -1;
+    //caloriesMatrix.forEach((elf, i)=> {
+        //let total = elf.reduce((acc, curr) => +curr + acc, 0);
+        //if(total > maxCalories) {
+            //elfWithMostCalories = i + 1;
+            //maxCalories = total;
+        //}
+    //});
+    //console.log(elfWithMostCalories, maxCalories);
+    //return maxCalories;
+//}
+
+//console.log("Reading Elfs items' calorie list...");
+//console.log("Processing data...")
+//maxCaloriesPerElf("calories.txt");
+
+
+// part2
+
+const top3MaxCalories = async (name) => {
+    let data = await fs.readFile(name, "utf-8");
+    const caloriesMatrix = data.split('\n\n').map(collection => collection.split('\n'));
+    const caloriesTotal = caloriesMatrix.map((elf, i)=> {
+        let total = elf.reduce((acc, curr) => +curr + acc, 0);
+        return total;
+    });
+    const top3 = caloriesTotal.sort((a,b) => b - a).slice(0, 3);
+    const top3Total = top3.reduce((acc, curr) => curr + acc, 0);
+    console.log(top3Total);
+    return top3Total;
+}
+
+top3MaxCalories("calories.txt");
+
+
