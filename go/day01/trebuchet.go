d11cf6403e052f470dd89e7e89993c7d63d0ed1b
Julian Rodriguez <92952502+juleanrod@users.noreply.github.com>
2023-12-02 15:17:12 -0800

    day01 part 2

--- go/day01/trebuchet.go
+++ go/day01/trebuchet.go
@@ -5,9 +5,9 @@ import (
 	"fmt"
 	"log"
 	"os"
-    "strconv"
+	"strconv"
+	"strings"
 )
-
 
 func main() {
 
@@ -42,23 +42,65 @@ func findFirstLastInt(line string) int {
 
     var first, second string
 
+    stoi := map[string]string {
+        "one": "1", "two": "2", "three": "3",
+        "four": "4", "five": "5", "six": "6",
+        "seven": "7", "eight": "8", "nine": "9",
+    }
+
+    m := map[byte][]string{
+        'o': {"one"},
+        't': {"two","three"},
+        'f': {"four", "five"},
+        's': {"six", "seven"},
+        'e': {"eight"},
+        'n': {"nine"},
+    }
+
+    // adding key, pairs to the map
+
     i, j := 0, len(line) - 1
     for i < len(line) {
         if isInteger(line[i]) == true {
             first = string(line[i])
             break
         }
+        if s, ok := m[line[i]]; ok == true {
+            lengthSlice := len(s)
+            for k := 0; k < lengthSlice; k++ {
+                current := s[k]
+                if strings.HasPrefix(line[i:], current) == true {
+                    first = stoi[current]
+                    i += len(current) - 1
+                    break
+                }
+            }
+        }
+        if(first != "") {
+            break
+        } 
         i++
-
     }
-
+    
     for j >= i {
         if isInteger(line[j]) == true {
             second = string(line[j])
             break
         }
+        if s, ok := m[line[j]]; ok == true {
+            lengthSlice := len(s)
+            for k := 0; k < lengthSlice; k++ {
+                current := s[k]
+                if(strings.HasPrefix(line[j:], current) == true) {
+                    second = stoi[current]
+                    break
+                }
+            }
+        }
+        if(second != "") {
+            break
+        }
         j--
-
     }
 
     valueInt, _ := strconv.Atoi(first + second)
